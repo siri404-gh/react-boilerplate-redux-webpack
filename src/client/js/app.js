ce875a3c81778796556305fc3a31bd6a062cc56f
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-02-08 18:50:11 +0000

    Develop (#1)
    * initial commit
    
    * Changed Page title.
    
    * Enabled CD at heroku and disabled at Travis.
    
    * Added deploy info as comments.
    
    * showing CI/CD to nattamai

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => (
-  <h1>Built using travis, deployed into heroku.</h1>
+  <h1>Teaching nattamai countinous integration countinous deployment.</h1>
 );
 
 ReactDOM.render(<App/>, document.getElementById('content'));
