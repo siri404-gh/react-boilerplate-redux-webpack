12d0c41616de2312bde7e0b76a53fb1607da05d2
Sreeram Padmanabhan <sreeram@L-156095610.local>
2017-02-08 20:47:41 +0000

    CI/CD demo to srini

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => (
-  <h1>Teaching nattamai countinous integration countinous deployment.</h1>
+  <h1>Teaching srini countinous integration countinous deployment.</h1>
 );
 
 ReactDOM.render(<App/>, document.getElementById('content'));
