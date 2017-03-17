05b0240e48f2cacffd5128fc2ea8135891612155
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-03-17 23:21:56 +0000

    Develop (#4)
    * updates
    
    * Componetized everything.
    
    * Eslint Changes
    
    * Separated into Container and Presentational components.
    
    * refactor: moved i into the class
    
    * feat: Introduced Commitizen
    
    * refactor: Added provider
    
    * refactor: Added more Containers
    
    * refactor: Seperated actions into actionCreators
    
    * feat: Added react-material, trying a new folder structure
    
    * Webpack (#3)
    
    * feat:
    
    * removed vscode files.
    
    * feat: webpack
    
    * refactor:
    
    * fix: fixed travis job
    
    * feat:
    
    * fix:
    
    * fix: changed heroku running port
    
    * feat:
    
    * feat:
    
    * fix:
    
    * fix:
    
    * fix: relative path for webpack
    
    * fix: why isnt heroku deploying properly?
    
    * updated readme
    
    * feat:
    
    * refactor:
    
    * refactor:

--- src/client/js/redux/index.js
+++ src/client/js/redux/index.js
@@ -1,27 +1,30 @@
 export const createStore = (reducer) => {
-  let state;
-  let listeners = [];
+    let state;
+    let listeners = [];
 
-  const getState = () => state;
-  const subscribe = (listener) => {
-    listeners.push(listener);
-  };
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    console.log('state changed to - ', state);
-    listeners.forEach((listener)=> {
-      listener();
-    });
-  };
-  dispatch({});
-  return { getState, subscribe, dispatch };
+    const getState = () => state;
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => listener !== l);
+        };
+    };
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        console.log('State changed to - ', state);
+        listeners.forEach((listener) => {
+            listener();
+        });
+    };
+    dispatch({});
+    return { getState, subscribe, dispatch };
 };
 
-export const combinedReducers = (reducers) => {
-  return (state = {}, action) => {
-    return Object.keys(reducers).reduce((nextState, key) => {
-      nextState[key] = reducers[key](state[key], action);
-      return nextState;
-    }, {});
-  };
+export const combineReducers = (reducers) => {
+    return (state = {}, action) => {
+        return Object.keys(reducers).reduce((nextState, key) => {
+            nextState[key] = reducers[key](state[key], action);
+            return nextState;
+        }, {});
+    };
 };
